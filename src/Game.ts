86494414a01010686bd831a67d3af2cea9031963
Elliot <elliotgriffiss@gmail.com>
2024-07-05 19:40:02 +0100

    Added the Background and some notes

--- src/Game.ts
+++ src/Game.ts
@@ -2,11 +2,27 @@ import {Container, Sprite, Text} from "pixi.js";
 import {sound} from '@pixi/sound';
 import gsap from "gsap";
 
+import Background from "./Components/Background/Background";
+
 import settings from './app.json';
 
 class Game extends Container {
     constructor() {
         super();
+
+        const background = new Background();
+
+        this.addChild(
+            background,
+        );
+
+        // Buttons,
+        // circle 3px
+        // 12 pixels padding each way,
+        // text 22 pixels,
+
+        // circle 3px
+        // text 48
     }
 }
 
