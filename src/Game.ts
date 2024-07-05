050f4d4bfa2fb49f894b95635bd910d992dc9c2f
Elliot <elliotgriffiss@gmail.com>
2024-07-05 18:09:32 +0100

    Added app.json

--- src/Game.ts
+++ src/Game.ts
@@ -2,6 +2,8 @@ import {Container, Sprite, Text} from "pixi.js";
 import {sound} from '@pixi/sound';
 import gsap from "gsap";
 
+import settings from './app.json';
+
 class Game extends Container {
     constructor() {
         super();
