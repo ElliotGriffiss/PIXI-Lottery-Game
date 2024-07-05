a3f2555eec6e9c174c7eeea5e328be75a16e22c3
Elliot <elliotgriffiss@gmail.com>
2024-07-05 19:54:17 +0100

    Added the SpinButton

---
+++ src/Components/SpinButton/SpinButton.ts
@@ -0,0 +1,15 @@
+import Button from "../../Engine/Button/Button";
+
+class SpinButton extends Button {
+    constructor(buttonPayload:() => void) {
+        super(buttonPayload, {
+            active: global.game.SpinButton_Active,
+            pressed: global.game.SpinButton_Pressed,
+            inactive: global.game.SpinButton_Inactive
+        });
+        this.x = 585;
+        this.y = 472;
+    }
+}
+
+export default SpinButton;
--- src/Game.ts
+++ src/Game.ts
@@ -3,17 +3,24 @@ import {sound} from '@pixi/sound';
 import gsap from "gsap";
 
 import Background from "./Components/Background/Background";
+import SpinButton from "./Components/SpinButton/SpinButton";
 
 import settings from './app.json';
 
 class Game extends Container {
+    private readonly _spinButton: SpinButton = null;
+
     constructor() {
         super();
 
         const background = new Background();
 
+
+        this._spinButton = new SpinButton(()=> {this._onSpinButtonPressed()});
+
         this.addChild(
             background,
+            this._spinButton
         );
 
         // Buttons,
@@ -23,6 +30,9 @@ class Game extends Container {
 
         // circle 3px
         // text 48
+    }
+
+    private _onSpinButtonPressed(): void {
     }
 }
 
